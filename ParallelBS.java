9f7acae90add83e41f44492d5a212e0cc212ca6a
NSel1727 <noahseligson18@gmail.com>
2023-01-29 11:21:13 -0500

    Add number generator

--- ParallelBS.java
+++ ParallelBS.java
@@ -1,9 +1,21 @@
 import java.util.*;
-import java.io.*;
 
 public class ParallelBS{
 
+    private static int[] testData;
+    private static int size;
+
     public static void main(String[] args){
+        size = getRandomNum();
+        testData = new int[size];
+
+        for(int i = 0; i < size; i++)
+            testData[i] = getRandomNum();
+
         
+    }
+
+    private static int getRandomNum(){
+        return (int) (Math.random() * 100);
     }
 }
