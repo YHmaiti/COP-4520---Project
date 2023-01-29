0ba3fc976ddc1ff892df73e41d93a950e4caa237
NSel1727 <noahseligson18@gmail.com>
2023-01-29 13:23:02 -0500

    Add algorithm

--- ParallelBS.java
+++ ParallelBS.java
@@ -1,18 +1,47 @@
 import java.util.*;
 
 public class ParallelBS{
-
-    private static int[] testData;
-    private static int size;
-
     public static void main(String[] args){
-        size = getRandomNum();
-        testData = new int[size];
+        for(int i = 0; i < 1000; i++){
+            int size = getRandomNum();
+            int[] testData = new int[size];
+    
+            for(int j = 0; j < size; j++)
+                testData[j] = getRandomNum();
+    
+            int[] sortedArray = testData.clone();
 
-        for(int i = 0; i < size; i++)
-            testData[i] = getRandomNum();
+            Arrays.sort(sortedArray);
+    
+            bubbleSort(testData);
+    
+            if(!Arrays.equals(sortedArray, testData))
+                System.out.println(Arrays.toString(testData));
+        }
+    }
 
-        
+    private static void bubbleSort(int[] nums){
+        int len = nums.length;
+
+        int even = (nums.length % 2 == 0) ? 1 : 0;
+
+        for(int j = 0; j < len - 1; j++){
+            int i;
+
+            for(i = 0; i < (len / 2) - even; i++)
+                if(nums[2*i+1] > nums[2*i+2])
+                    swap(nums, 2*i+1, 2*i+2);
+
+            for(i = 0; i < (len / 2); i++)
+                if(nums[2*i] > nums[2*i+1])
+                    swap(nums, 2*i, 2*i+1);
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 
     private static int getRandomNum(){
