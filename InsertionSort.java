7021cb01dcb5998f4d4d73f5bcf1a667b7b0ebb2
Melanie Ehrlich <melanie6104@icloud.com>
2023-02-16 13:27:30 -0500

    insertion sort code

--- InsertionSort.java
+++ InsertionSort.java
@@ -39,33 +39,24 @@ public class InsertionSort {
     // parallel insertion sort
     public static Runnable parallel(int arr[], int min, int max) {
 
+        if (max <= arr.length) {
+            max = arr.length - 1;
+        }
+
         // have multiple parts of the array
         // sorted on different threads
-
-        // max is the number of the maximum index this thread
-        // will sort from the array
-        // min is the minimum index
-
-        // loop through the array to the max index
-        for (int i = 0; i < max; ++i) {
+        for (int i = 0; i <= max; ++i) {
 
             // the current item
             int target = arr[i];
             int j = i - 1;
 
-            // this sort runs in quadratic time, o(n^2)
-            // for every item, we go for elements starting
-            // from the current item
-            // and moving backwards to the start of the array
             while (j >= 0 && arr[j] > target) {
 
-                // if the element is smaller than target it
-                // does not need to move
                 arr[j + 1] = arr[j];
                 j = j - 1;
             }
-            // all items to the left of target
-            // should be smaller than it
+
             arr[j + 1] = target;
         }
 
@@ -78,19 +69,15 @@ public class InsertionSort {
     // driver function
     public static void main(String[] args) {
 
-        // System.out.println("Max processors: " +
-        // Runtime.getRuntime().availableProcessors());
-
         long pStart = 0; // the times for the parallel version
         long pEnd = 0;
         long pDuration = 0;
         long sStart = 0; // the times for the sequential version
         long sEnd = 0;
         long sDuration = 0;
-        int max = 0; // used for the maximum index number of the array each thread will compute
-        int min = 0; // the minimum index number of the array a thread will compute
-        int threads = 8; // the number of threads to be ran
-        int n = 10; // the factor to increase each thread's indexes by
+        int max = 0;
+        int min = 0;
+        int threads = 12;
 
         // create an array of random ints for testing
         Random rand = new Random();
@@ -103,16 +90,6 @@ public class InsertionSort {
 
         }
 
-        /*
-         * System.out.println("----------Array before sorting----------");
-         * 
-         * for (int i = 0; i < arr.length; i++) {
-         * 
-         * System.out.println(arr[i]);
-         * 
-         * }
-         */
-
         // duplicate the array to test with iterative version
         int[] itArray = new int[arr.length];
 
@@ -121,11 +98,6 @@ public class InsertionSort {
             itArray[i] = arr[i];
 
         }
-
-        // prompt the user for the number of threads
-        // System.out.println("Enter the number of threads to be ran (2, 4, 8, 12):");
-        // Scanner sc = new Scanner(System.in);
-        // threads = sc.nextInt();
 
         // create an array containing multiple threads
         Thread[] thread = new Thread[threads];
@@ -167,35 +139,12 @@ public class InsertionSort {
 
         pDuration = pEnd - pStart;
 
-        // System.out.println("----------Array after parallel sort----------");
-
-        /*
-         * for (int i = 0; i < arr.length; i++) {
-         * 
-         * System.out.println(arr[i]);
-         * 
-         * }
-         */
-
-        // close the scanner
-        // sc.close();
-
         // run sequential version to compare
         sStart = System.currentTimeMillis();
         InsertionSort ob = new InsertionSort();
         ob.sequential(itArray);
         sEnd = System.currentTimeMillis();
         sDuration = sEnd - sStart;
-
-        // System.out.println("----------Array after sequential sort----------");
-
-        /*
-         * for (int i = 0; i < itArray.length; i++) {
-         * 
-         * System.out.println(itArray[i]);
-         * 
-         * }
-         */
 
         System.out.println("Duration of parallel insertion sort with " + arr.length + " elements: " + pDuration);
         System.out.println("Duration of sequential insertion sort with " + arr.length + " elements: " + sDuration);
