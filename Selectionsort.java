f8837a645d8088ff57fafdbc2ea8651470c2f66b
MohammadA8 <mohammadhawani8@gmail.com>
2023-02-22 22:38:44 -0500

    parallel implemention of selection sort

--- Selectionsort.java
+++ Selectionsort.java
@@ -1,4 +1,45 @@
+import java.util.HashMap;
 import java.util.Random;
+
+class Utility
+{
+
+    public int min1Index;
+    public int max1Index;
+    public int min2Index;
+    public int max2Index;
+
+    public int min1;
+    public int max1;
+    public int min2;
+    public int max2;
+
+    public Utility()
+    {
+        min1 = Integer.MAX_VALUE;
+        max1 = Integer.MIN_VALUE;
+        min2 = Integer.MAX_VALUE;
+        max2 = Integer.MIN_VALUE;
+
+    }
+
+}
+
+class Boundries
+{
+
+    public int start;
+    public int end;
+
+    public Boundries(int start, int end)
+    {
+        this.start = start;
+        this.end = end;
+
+    }
+
+}
+
 
 public class Selectionsort {   
     
@@ -6,91 +47,106 @@ public class Selectionsort {
     {
         int size = arr.length;
         int mid = size / 2;
-        int start = 0;
-        int end = size - 1;
 
-        while (start < end)
+        Boundries bound = new Boundries(0, size -1);
+
+        while (bound.start < bound.end)
         {
+            
 
             Thread[] threads = new Thread[2];
 
+            Utility util = new Utility();
+          
+            threads[0] = new Thread(() -> {
 
-            int min1Index = 0;
-            int max1Index = 0;
-            int min2Index = 0;
-            int max2Index = 0;
+                int startT = bound.start;
+                for (int i = startT; i < mid; i++)
+                {
 
-            int min1 = Integer.MAX_VALUE;
-            int max1 = Integer.MIN_VALUE;
-            int min2 = Integer.MAX_VALUE;
-            int max2 = Integer.MIN_VALUE;
+    
+                    if (arr[i] < util.min1)
+                    {
+                        util.min1 = arr[i];
+                        util.min1Index = i;
+                    }
+    
+                    if (arr[i] > util.max1)
+                    {
+                        util.max1 = arr[i];
+                        util.max1Index = i;
+                    }
+                        
+    
+                }               
             
-            for (int i = start; i < mid; i++)
-            {
+            });
 
-                if (arr[i] < min1)
+            threads[0].start();
+
+            threads[1] = new Thread(() -> {
+
+                for (int j = mid; j <= bound.end; j++)
                 {
-                    min1 = arr[i];
-                    min1Index = i;
-                }
+    
+                    if (arr[j] < util.min2)
+                    {
+                        util.min2 = arr[j];
+                        util.min2Index = j;
+                    }
+    
+                    if (arr[j] > util.max2)
+                    {
+                        util.max2 = arr[j];
+                        util.max2Index = j;
+                    }
+                        
+    
+                }             
+            
+            });
 
-                if (arr[i] > max1)
-                {
-                    max1 = arr[i];
-                    max1Index = i;
-                }
-                    
+            threads[1].start();
 
+            for (Thread thread : threads) {
+                try {
+                    thread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
 
-            for (int j = mid; j <= end; j++)
+
+            // System.out.println("min1 is " + util.min1 +  " min2 is " + util.min2 + " max1 is " + util.max1 + " max2 is " + util.max2);
+
+            if (util.min1 < util.min2)
             {
-
-                if (arr[j] < min2)
-                {
-                    min2 = arr[j];
-                    min2Index = j;
-                }
-
-                if (arr[j] > max2)
-                {
-                    max2 = arr[j];
-                    max2Index = j;
-                }
-                    
-
-            }
-
-            System.out.println("min1 is " + min1 +  " min2 is " + min2 + " max1 is " + max1 + " max2 is " + max2);
-
-            if (min1 < min2)
-            {
-                if (max1Index == start)
-                    max1Index = min1Index;
-                swap(arr, min1Index, start);
+                if (util.max1Index == bound.start)
+                    util.max1Index = util.min1Index;
+                swap(arr, util.min1Index, bound.start);
                 
             }
             else
             {
-                if (max1Index == start)
-                    max1Index = min2Index;
-                swap(arr, min2Index, start);
+                if (util.max1Index == bound.start)
+                    util.max1Index = util.min2Index;
+                swap(arr, util.min2Index, bound.start);
             }
 
-            if (end - start > 1)
+            if (bound.end - bound.start > 1)
             {
-                if (max1 > max2)
-                    swap(arr, max1Index, end);
+                if (util.max1 > util.max2)
+                    swap(arr, util.max1Index, bound.end);
                 else
-                    swap(arr, max2Index, end);
+                    swap(arr, util.max2Index, bound.end);
             }
 
-            for(int i:arr){  
-                System.out.print(" -" + i + "- ");        }  
-            System.out.println();
+            // for(int i:arr){  
+            //     System.out.print(" -" + i + "- ");        }  
+            // System.out.println();
 
-            start++;
-            end --;
+            bound.start++;
+            bound.end --;
 
         }
 
@@ -134,11 +190,11 @@ public class Selectionsort {
        
     public static void main(String a[]){  
         Random random = new Random();
-        int[] arr1 = random.ints(10, 10,1000).toArray();
+        int[] arr1 = random.ints(200000, 10,10000).toArray();
 
-        System.out.println("before Selection Sort");  
-        for(int i:arr1){  
-            System.out.print(" -" + i + "- ");        }  
+        // System.out.println("before Selection Sort");  
+        // for(int i:arr1){  
+        //     System.out.print(" -" + i + "- ");        }  
         System.out.println();
 
         long startTime = System.nanoTime();
@@ -147,10 +203,10 @@ public class Selectionsort {
         System.out.println("\n" + (isSorted(arr1) ? "Array sorted successfully" : "unsuccessful sorting"));
         long endTime = System.nanoTime();
 
-        System.out.println("After Selection Sort");  
-        for(int i:arr1){  
-            System.out.print(" -" + i + "- ");  
-        }  
+        // System.out.println("After Selection Sort");  
+        // for(int i:arr1){  
+        //     System.out.print(" -" + i + "- ");  
+        // }  
 
 
         System.out.println("\nTime it took to execute parallel selection sort: " + String.format("%.4f",((double)(endTime - startTime) / 1000000)/1000) + " seconds" );
