0c04adc9799924fc7223598ea7e8344660ed28bf
MohammadA8 <mohammadhawani8@gmail.com>
2023-03-07 21:27:00 -0500

    finished select sort setup

--- MultithreadSelectionSort/Selectionsort.java
+++ MultithreadSelectionSort/Selectionsort.java
@@ -2,8 +2,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.Stack;
 
 class Utility
@@ -48,7 +50,7 @@ class Boundries
 
 public class Selectionsort {
     
-    public static void MMBPSS(int arr[]) // MMBPSS is for Min-Max Bidirectional Parallel Selection Sort
+    public static void MMBPSS(Integer arr[]) // MMBPSS is for Min-Max Bidirectional Parallel Selection Sort
     {
         int size = arr.length;
         int mid = size / 2;
@@ -155,7 +157,7 @@ public class Selectionsort {
 
     }
 
-    public static void swap(int[] arr, int indexA, int indexB)
+    public static void swap(Integer[] arr, int indexA, int indexB)
     {
 
         int temp = arr[indexA];
@@ -165,7 +167,7 @@ public class Selectionsort {
     }
 
     // =================================================
-    public static void selectionSort(int[] arr){  
+    public static void selectionSort(Integer[] arr){  
         for (int i = 0; i < arr.length - 1; i++)  
         {  
             int index = i;  
@@ -181,7 +183,7 @@ public class Selectionsort {
         }  
     } 
     
-    public static void dynamicSelectionSort(int[] arr){  
+    public static void dynamicSelectionSort(Integer[] arr){  
 
         Stack<Integer> minStack = new Stack<>();
         HashMap<Integer, Integer> indexMap = new HashMap<>();
@@ -212,7 +214,7 @@ public class Selectionsort {
         }  
     }  
 
-    public static boolean isSorted(int[] a) {
+    public static boolean isSorted(Integer[] a) {
 
             for (int i = 0; i < a.length - 1; i++) {
                 if (a[i] > a[i + 1]) {
@@ -259,29 +261,219 @@ public class Selectionsort {
         //     System.out.print(" -" + i + "- ");  
         // }  
 
-        // File file = new File("");
 
-        // try {
-    
-        //     Scanner sc = new Scanner(file);
-    
-        //     while (sc.hasNextLine()) {
-        //         int i = sc.nextInt();
-        //         System.out.println(i);
-        //     }
-        //     sc.close();
-        // } 
-        // catch (FileNotFoundException e) {
-        //     e.printStackTrace();
-        // }
+
 
         File file = new File("SelectionSortParallel_Results\\Results.txt");
         PrintWriter writer;
         try {
             writer = new PrintWriter("SelectionSortParallel_Results\\Results.txt", "UTF-8");
+            writer.println("Content");
 
-            writer.println("The first line");
-            writer.println("The second line");
+            File testFile = new File("..\\TestCases\\SmallTC1.txt");
+            ArrayList<Integer> arrayList = new ArrayList<>();
+    
+            try {
+        
+                Scanner sc = new Scanner(testFile);
+        
+                while (sc.hasNextInt()) {
+                    int i = sc.nextInt();
+                    arrayList.add(i);
+                }
+                sc.close();
+            } 
+            catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+    
+            Integer [] SmallTC1 = new Integer[arrayList.size()];
+            arrayList.toArray(SmallTC1);
+            writer.println("Test case size: " + SmallTC1.length);
+
+            Long beforeUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+            Long startTime = System.nanoTime();
+            MMBPSS(SmallTC1);
+            Long endTime = System.nanoTime();
+            Long afterUsedMem= Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+            Long actualMemUsed= afterUsedMem - beforeUsedMem;
+            // System.out.println("\n" + (isSorted(SmallTC1) ? "Array sorted successfully" : "unsuccessful sorting"));
+
+            writer.println("    Execution time (ms): " + String.format("%.4f",((double)(endTime - startTime) / 1000000)));
+            writer.println("	Memory usage (bytes): " + actualMemUsed);
+            writer.println("");
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            testFile = new File("..\\TestCases\\SmallTC2.txt");
+            arrayList = new ArrayList<>();
+    
+            try {
+        
+                Scanner sc = new Scanner(testFile);
+        
+                while (sc.hasNextInt()) {
+                    int i = sc.nextInt();
+                    arrayList.add(i);
+                }
+                sc.close();
+            } 
+            catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+    
+            Integer [] SmallTC2 = new Integer[arrayList.size()];
+            arrayList.toArray(SmallTC2);
+            writer.println("Test case size: " + SmallTC2.length);
+
+            beforeUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+            startTime = System.nanoTime();
+            MMBPSS(SmallTC2);
+            endTime = System.nanoTime();
+            afterUsedMem= Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+            actualMemUsed= afterUsedMem - beforeUsedMem;
+            // System.out.println("\n" + (isSorted(SmallTC2) ? "Array sorted successfully" : "unsuccessful sorting"));
+
+            writer.println("    Execution time (ms): " + String.format("%.4f",((double)(endTime - startTime) / 1000000)));
+            writer.println("	Memory usage (bytes): " + actualMemUsed);
+            writer.println("");
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            testFile = new File("..\\TestCases\\MediumTC1.txt");
+            arrayList = new ArrayList<>();
+    
+            try {
+        
+                Scanner sc = new Scanner(testFile);
+        
+                while (sc.hasNextInt()) {
+                    int i = sc.nextInt();
+                    arrayList.add(i);
+                }
+                sc.close();
+            } 
+            catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+    
+            Integer [] MediumTC1 = new Integer[arrayList.size()];
+            arrayList.toArray(MediumTC1);
+            writer.println("Test case size: " + MediumTC1.length);
+
+            beforeUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+            startTime = System.nanoTime();
+            MMBPSS(MediumTC1);
+            endTime = System.nanoTime();
+            afterUsedMem= Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+            actualMemUsed= afterUsedMem - beforeUsedMem;
+            // System.out.println("\n" + (isSorted(MediumTC1) ? "Array sorted successfully" : "unsuccessful sorting"));
+
+            writer.println("    Execution time (ms): " + String.format("%.4f",((double)(endTime - startTime) / 1000000)));
+            writer.println("	Memory usage (bytes): " + actualMemUsed);
+            writer.println("");
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            testFile = new File("..\\TestCases\\MediumTC2.txt");
+            arrayList = new ArrayList<>();
+    
+            try {
+        
+                Scanner sc = new Scanner(testFile);
+        
+                while (sc.hasNextInt()) {
+                    int i = sc.nextInt();
+                    arrayList.add(i);
+                }
+                sc.close();
+            } 
+            catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+    
+            Integer [] MediumTC2 = new Integer[arrayList.size()];
+            arrayList.toArray(MediumTC2);
+            writer.println("Test case size: " + MediumTC2.length);
+
+            beforeUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+            startTime = System.nanoTime();
+            MMBPSS(MediumTC2);
+            endTime = System.nanoTime();
+            afterUsedMem= Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+            actualMemUsed= afterUsedMem - beforeUsedMem;
+            // System.out.println("\n" + (isSorted(MediumTC2) ? "Array sorted successfully" : "unsuccessful sorting"));
+
+            writer.println("    Execution time (ms): " + String.format("%.4f",((double)(endTime - startTime) / 1000000)));
+            writer.println("	Memory usage (bytes): " + actualMemUsed);
+            writer.println("");
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//             testFile = new File("..\\TestCases\\LargeTC1.txt");
+//             arrayList = new ArrayList<>();
+    
+//             try {
+        
+//                 Scanner sc = new Scanner(testFile);
+        
+//                 while (sc.hasNextInt()) {
+//                     int i = sc.nextInt();
+//                     arrayList.add(i);
+//                 }
+//                 sc.close();
+//             } 
+//             catch (FileNotFoundException e) {
+//                 e.printStackTrace();
+//             }
+    
+//             Integer [] LargeTC1 = new Integer[arrayList.size()];
+//             arrayList.toArray(LargeTC1);
+//             writer.println("Test case size: " + LargeTC1.length);
+
+//             beforeUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+//             startTime = System.nanoTime();
+//             MMBPSS(LargeTC1);
+//             endTime = System.nanoTime();
+//             afterUsedMem= Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+//             actualMemUsed= afterUsedMem - beforeUsedMem;
+//             // System.out.println("\n" + (isSorted(LargeTC1) ? "Array sorted successfully" : "unsuccessful sorting"));
+
+//             writer.println("    Execution time (ms): " + String.format("%.4f",((double)(endTime - startTime) / 1000000)));
+//             writer.println("	Memory usage (bytes): " + actualMemUsed);
+//             writer.println("");
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//             testFile = new File("..\\TestCases\\LargeTC2.txt");
+//             arrayList = new ArrayList<>();
+    
+//             try {
+        
+//                 Scanner sc = new Scanner(testFile);
+        
+//                 while (sc.hasNextInt()) {
+//                     int i = sc.nextInt();
+//                     arrayList.add(i);
+//                 }
+//                 sc.close();
+//             } 
+//             catch (FileNotFoundException e) {
+//                 e.printStackTrace();
+//             }
+    
+//             Integer [] LargeTC2 = new Integer[arrayList.size()];
+//             arrayList.toArray(LargeTC2);
+//             writer.println("Test case size: " + LargeTC2.length);
+
+//             beforeUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+//             startTime = System.nanoTime();
+//             MMBPSS(LargeTC2);
+//             endTime = System.nanoTime();
+//             afterUsedMem= Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+//             actualMemUsed= afterUsedMem - beforeUsedMem;
+//             // System.out.println("\n" + (isSorted(LargeTC2) ? "Array sorted successfully" : "unsuccessful sorting"));
+
+//             writer.println("    Execution time (ms): " + String.format("%.4f",((double)(endTime - startTime) / 1000000)));
+//             writer.println("	Memory usage (bytes): " + actualMemUsed);
+//             writer.println("");
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
             writer.close();
 
         } catch (FileNotFoundException | UnsupportedEncodingException e) {
